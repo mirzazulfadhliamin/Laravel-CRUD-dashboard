51591d0f2fa96d81c8bb3bf906d822b0ba2f2f10
unknown <mirzazulfadhli99@gmail.com>
2023-10-10 11:24:59 +0700

    create-project

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends("main")
+
+@section('content')
+<div class="container mt-4">
+    <h1>Welcome to My about</h1>
+    <p>This is a simple homepage created using Bootstrap.</p>
+</div>
+@endsection
---
+++ resources/views/student.blade.php
@@ -0,0 +1,8 @@
+@extends("main")
+
+@section('content')
+<div class="container mt-4">
+    <h1>Welcome to My student</h1>
+    <p>This is a simple homepage created using Bootstrap.</p>
+</div>
+@endsection
